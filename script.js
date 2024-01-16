cf33cde869bfb47db492bedddcef20400df0763b
PritamKumarSaha <137249952+PritamKumarSaha@users.noreply.github.com>
2024-01-16 16:30:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const student = {
+	name: "Pritam"
+};
+
+Object.prototype.getKeys = function() {
+	return Object.keys(this);
+};
+
+const keyArray = student.getKeys();
+
+console.log(keyArray);
+
